23dcfd9efacf7ef473ecc8e9016ccca7b87568c8
brady <bradysuryasie@gmail.com>
2022-10-21 15:31:53 +0700

    some big changes

---
+++ utilities/retrieveAllFilesInDirectory.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const fs = require("fs");
+
+console.log(path.join(__dirname, "Documents"))
+
+// For every file in screens
+// Get the default export for all of them
+// The name of the file is the name of the screen
